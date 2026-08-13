a553202de7607f99a541b1c5e99f0b59bea55198
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-08-13 12:58:45 -0300

    Inserindo definições de constantes (pré-processadas)

---
+++ Trabalho/Caio.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXIMO_LINHAS 40
+#define MAXIMO_COLUNAS 100
+
+#define ESPACO ' '
+#define PAREDE '#'
+#define COMIDA '*'
+#define TUNEL '@'
+
+#define PACMAN '>'
+#define FANTASMA_B 'B'
+#define FANTASMA_P 'P'
+#define FANTASMA_I 'I'
+#define FANTASMA_C 'C'
+
+#define MOVIMENTO_PARA_ESQUERDO 'a'
+#define MOVIMENTO_PARA_DIREITA 'd'
+#define MOVIMENTO_PARA_CIMA 'w'
+#define MOVIMENTO_PARA_BAIXO 's'
+
+
+
+int main() {
+
+    return 0;
+}
